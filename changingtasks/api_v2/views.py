a239638adad98eecdf37e1213f981baf196a292f
Oleg <slepcov_oleg@mail.ru>
2022-08-24 14:24:31 +0700

    Добавление обработчика события - создание задачи

--- changingtasks/api_v2/views.py
+++ changingtasks/api_v2/views.py
@@ -35,13 +35,76 @@ class InstallApiView(views.APIView):
 
 
 class TaskCreateApiView(views.APIView):
-    pass
+    def post(self, request):
+        logger_error.info({
+            "request.query_params": request.query_params,
+            "request.data": request.data
+        })
+        # task_id = request.query_params.get("data[FIELDS_AFTER][ID]", "")
+        task_id = request.data.get("data[FIELDS_AFTER][ID]", "")
+        application_token = request.data.get("auth[application_token]", None)
+
+        if application_token != APPLICATION_TOKEN:
+            return Response("Unverified event source", status=status.HTTP_400_BAD_REQUEST)
+
+        if not task_id:
+            return Response("Not transferred ID task", status=status.HTTP_400_BAD_REQUEST)
+
+        # получение данных сущности - задача
+        result_task = service_func.get_task_data(task_id)
+        if not result_task or "result" not in result_task or "task" not in result_task["result"]:
+            logger_error.error({
+                "event": "TaskUpdateApiView",
+                "task_id": task_id,
+                "response": result_task,
+                "message": "Не удалось получить данные задачи",
+            })
+            return Response("No response from bitrix", status=status.HTTP_400_BAD_REQUEST)
+
+        task = result_task["result"]["task"]
+        if "ufCrmTask" not in task:
+            logger_error.error({
+                "event": "TaskUpdateApiView",
+                "result_task": result_task["result"],
+                "message": "Отсутствует поле с привязками задачи к CRM сущностям",
+            })
+            return Response("The task is not tied to the crm entity", status=status.HTTP_400_BAD_REQUEST)
+
+        id_deal = service_func.get_id_deal_from_line_str(task["ufCrmTask"], "D")
+        if not id_deal:
+            logger_error.error({
+                "event": "TaskUpdateApiView",
+                "id_deal": task["ufCrmTask"],
+                "message": "Отсутствует привязка задачи к сделке",
+            })
+            return Response("The task is not linked to the deal", status=status.HTTP_400_BAD_REQUEST)
+
+        # получение данных сущности - сделка
+        result_deal = service_func.get_deal_data(id_deal)
+        if not result_deal or "result" not in result_deal:
+            logger_error.error({
+                "event": "TaskUpdateApiView",
+                "id_deal": id_deal,
+                "response": result_deal,
+                "message": "Не удалось получить данные сделки",
+            })
+            return Response("No response from bitrix", status=status.HTTP_400_BAD_REQUEST)
+
+        deal = result_deal["result"]
+
+        # проброс комментариев в задачу
+        service_func.throwing_comments(task, deal)
+
+        return Response("OK", status=status.HTTP_200_OK)
 
 
 class TaskUpdateApiView(views.APIView):
-    bx24 = bitrix24.Bitrix24()
 
     def post(self, request):
+        logger_error.info({
+            "request.query_params": request.query_params,
+            "request.data": request.data
+        })
         # task_id = request.query_params.get("data[FIELDS_AFTER][ID]", "")
         task_id = request.data.get("data[FIELDS_AFTER][ID]", "")
         application_token = request.data.get("auth[application_token]", None)
